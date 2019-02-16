87ce7133821969b9375cd1857fd549409335509b
Andrew Heppner <andrewbheppner@gmail.com>
2019-02-16 15:05:29 -0500

    Remove p5 animation to test build.

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,77 +29,79 @@ const Box = styled.div`
   // transition: all linear 0s;
 `;
 
-function sketch(p) {
-  let points;
-  let noiseScaleX;
-  let noiseScaleY;
-  let stepsPerFrame;
+// function sketch(p) {
+//   let points;
+//   let noiseScaleX;
+//   let noiseScaleY;
+//   let stepsPerFrame;
 
-  p.setup = () => {
-    stepsPerFrame = 3;
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    p.noFill();
-    p.strokeWeight(6);
-    p.stroke("#8B97B9");
-    reset();
-  };
+//   p.setup = () => {
+//     stepsPerFrame = 3;
+//     p.createCanvas(p.windowWidth, p.windowHeight);
+//     p.noFill();
+//     p.strokeWeight(6);
+//     p.stroke("#8B97B9");
+//     reset();
+//   };
 
-  p.draw = () => {
-    for (let i = 0; i < stepsPerFrame; i++) {
-      drawLine();
-    }
-  };
+//   p.draw = () => {
+//     for (let i = 0; i < stepsPerFrame; i++) {
+//       drawLine();
+//     }
+//   };
 
-  function reset() {
-    p.background("#BA6446");
-    noiseScaleX = p.random(0.001, 0.009);
-    noiseScaleY = p.random(0.004, 0.009);
-    resetPoints();
-  }
+//   function reset() {
+//     p.background("#BA6446");
+//     noiseScaleX = p.random(0.001, 0.009);
+//     noiseScaleY = p.random(0.004, 0.009);
+//     resetPoints();
+//   }
 
-  function resetPoints() {
-    points = [];
-    for (let i = 0; i < p.windowWidth; i++) {
-      points.push([i, 0]);
-    }
-  }
+//   function resetPoints() {
+//     points = [];
+//     for (let i = 0; i < p.windowWidth; i++) {
+//       points.push([i, 0]);
+//     }
+//   }
 
-  function drawLine() {
-    p.beginShape();
-    let atLeastOneOnScreen = false;
-    points.forEach(point => {
-      p.vertex(point[0], point[1]);
-      point[1] += p.noise(
-        point[0] * 1.1 * noiseScaleX,
-        p.frameCount * 0.001 * noiseScaleY
-      );
-      if (point[1] < p.windowHeight * 1.1) {
-        atLeastOneOnScreen = true;
-      }
-    });
-    p.endShape();
-    if (!atLeastOneOnScreen) {
-      reset();
-    }
-  }
-}
+//   function drawLine() {
+//     p.beginShape();
+//     let atLeastOneOnScreen = false;
+//     points.forEach(point => {
+//       p.vertex(point[0], point[1]);
+//       point[1] += p.noise(
+//         point[0] * 1.1 * noiseScaleX,
+//         p.frameCount * 0.001 * noiseScaleY
+//       );
+//       if (point[1] < p.windowHeight * 1.1) {
+//         atLeastOneOnScreen = true;
+//       }
+//     });
+//     p.endShape();
+//     if (!atLeastOneOnScreen) {
+//       reset();
+//     }
+//   }
+// }
 
 class IndexPage extends Component {
   state = {
     scrollY: null
+    sketch: null;
   };
 
   updateScroll = () => {
     this.setState({ scrollY: window.scrollY });
   };
 
-  // componentDidMount() {
-  //   window.addEventListener("scroll", () => this.updateScroll());
-  // }
+  componentDidMount() {
+    window.addEventListener("scroll", () => this.updateScroll());
+  }
 
-  // componentWillUnmount() {
-  //   window.addEventListener("scroll", () => this.updateScroll());
-  // }
+  componentWillUnmount() {
+    window.addEventListener("scroll", () => this.updateScroll());
+  }
+
   render() {
     console.log(this.state.scrollY, "scrollY");
     return (
@@ -107,7 +109,7 @@ class IndexPage extends Component {
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
         <MainWrapper>
           <Canvas>
-            <P5Wrapper sketch={sketch} />
+            {/* <P5Wrapper sketch={sketch} /> */}
           </Canvas>
           <BiddefordSeal />
           <Box top={5 * this.state.scrollY} />
