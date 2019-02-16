a8a01ef36aebea549221276720b597e4b4ace150
Andrew Heppner <andrewbheppner@gmail.com>
2019-02-16 15:15:55 -0500

    Move definition of sketch p5 function into component did Mount.

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,64 +30,12 @@ const Box = styled.div`
   // transition: all linear 0s;
 `;
 
-function sketch(p) {
-  let points;
-  let noiseScaleX;
-  let noiseScaleY;
-  let stepsPerFrame;
 
-  p.setup = () => {
-    stepsPerFrame = 3;
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    p.noFill();
-    p.strokeWeight(6);
-    p.stroke("#8B97B9");
-    reset();
-  };
-
-  p.draw = () => {
-    for (let i = 0; i < stepsPerFrame; i++) {
-      drawLine();
-    }
-  };
-
-  function reset() {
-    p.background("#BA6446");
-    noiseScaleX = p.random(0.001, 0.009);
-    noiseScaleY = p.random(0.004, 0.009);
-    resetPoints();
-  }
-
-  function resetPoints() {
-    points = [];
-    for (let i = 0; i < p.windowWidth; i++) {
-      points.push([i, 0]);
-    }
-  }
-
-  function drawLine() {
-    p.beginShape();
-    let atLeastOneOnScreen = false;
-    points.forEach(point => {
-      p.vertex(point[0], point[1]);
-      point[1] += p.noise(
-        point[0] * 1.1 * noiseScaleX,
-        p.frameCount * 0.001 * noiseScaleY
-      );
-      if (point[1] < p.windowHeight * 1.1) {
-        atLeastOneOnScreen = true;
-      }
-    });
-    p.endShape();
-    if (!atLeastOneOnScreen) {
-      reset();
-    }
-  }
-}
 
 class IndexPage extends Component {
   state = {
-    scrollY: null
+    scrollY: null,
+    sketch: null,
   };
 
   updateScroll = () => {
@@ -96,6 +44,62 @@ class IndexPage extends Component {
 
   componentDidMount() {
     window.addEventListener("scroll", () => this.updateScroll());
+
+    const sketch = (p) => {
+      let points;
+      let noiseScaleX;
+      let noiseScaleY;
+      let stepsPerFrame;
+    
+      p.setup = () => {
+        stepsPerFrame = 3;
+        p.createCanvas(p.windowWidth, p.windowHeight);
+        p.noFill();
+        p.strokeWeight(6);
+        p.stroke("#8B97B9");
+        reset();
+      };
+    
+      p.draw = () => {
+        for (let i = 0; i < stepsPerFrame; i++) {
+          drawLine();
+        }
+      };
+    
+      function reset() {
+        p.background("#BA6446");
+        noiseScaleX = p.random(0.001, 0.009);
+        noiseScaleY = p.random(0.004, 0.009);
+        resetPoints();
+      }
+    
+      function resetPoints() {
+        points = [];
+        for (let i = 0; i < p.windowWidth; i++) {
+          points.push([i, 0]);
+        }
+      }
+    
+      function drawLine() {
+        p.beginShape();
+        let atLeastOneOnScreen = false;
+        points.forEach(point => {
+          p.vertex(point[0], point[1]);
+          point[1] += p.noise(
+            point[0] * 1.1 * noiseScaleX,
+            p.frameCount * 0.001 * noiseScaleY
+          );
+          if (point[1] < p.windowHeight * 1.1) {
+            atLeastOneOnScreen = true;
+          }
+        });
+        p.endShape();
+        if (!atLeastOneOnScreen) {
+          reset();
+        }
+      }
+    }
+    this.setState({sketch});
   }
 
   componentWillUnmount() {
