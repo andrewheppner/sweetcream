9783a6c6304419573d8b0ab4f199cb9934687baa
Andrew Heppner <andrewbheppner@gmail.com>
2019-02-17 11:51:10 -0500

    Get P5 loading/building on server.

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,6 +29,61 @@ const Box = styled.div`
   // transition: all linear 0s;
 `;
 
+const sketch = p => {
+  let points;
+  let noiseScaleX;
+  let noiseScaleY;
+  let stepsPerFrame;
+
+  p.setup = () => {
+    stepsPerFrame = 3;
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.noFill();
+    p.strokeWeight(6);
+    p.stroke("#8B97B9");
+    reset();
+  };
+
+  p.draw = () => {
+    for (let i = 0; i < stepsPerFrame; i++) {
+      drawLine();
+    }
+  };
+
+  function reset() {
+    p.background("#BA6446");
+    noiseScaleX = p.random(0.001, 0.009);
+    noiseScaleY = p.random(0.004, 0.009);
+    resetPoints();
+  }
+
+  function resetPoints() {
+    points = [];
+    for (let i = 0; i < p.windowWidth; i++) {
+      points.push([i, 0]);
+    }
+  }
+
+  function drawLine() {
+    p.beginShape();
+    let atLeastOneOnScreen = false;
+    points.forEach(point => {
+      p.vertex(point[0], point[1]);
+      point[1] += p.noise(
+        point[0] * 1.1 * noiseScaleX,
+        p.frameCount * 0.001 * noiseScaleY
+      );
+      if (point[1] < p.windowHeight * 1.1) {
+        atLeastOneOnScreen = true;
+      }
+    });
+    p.endShape();
+    if (!atLeastOneOnScreen) {
+      reset();
+    }
+  }
+};
+
 class IndexPage extends Component {
   state = {
     scrollY: null,
@@ -43,60 +98,6 @@ class IndexPage extends Component {
     this.P5Wrapper = require("react-p5-wrapper");
     if (typeof window !== "undefined") {
       window.addEventListener("scroll", () => this.updateScroll());
-      const sketch = p => {
-        let points;
-        let noiseScaleX;
-        let noiseScaleY;
-        let stepsPerFrame;
-
-        p.setup = () => {
-          stepsPerFrame = 3;
-          p.createCanvas(p.windowWidth, p.windowHeight);
-          p.noFill();
-          p.strokeWeight(6);
-          p.stroke("#8B97B9");
-          reset();
-        };
-
-        p.draw = () => {
-          for (let i = 0; i < stepsPerFrame; i++) {
-            drawLine();
-          }
-        };
-
-        function reset() {
-          p.background("#BA6446");
-          noiseScaleX = p.random(0.001, 0.009);
-          noiseScaleY = p.random(0.004, 0.009);
-          resetPoints();
-        }
-
-        function resetPoints() {
-          points = [];
-          for (let i = 0; i < p.windowWidth; i++) {
-            points.push([i, 0]);
-          }
-        }
-
-        function drawLine() {
-          p.beginShape();
-          let atLeastOneOnScreen = false;
-          points.forEach(point => {
-            p.vertex(point[0], point[1]);
-            point[1] += p.noise(
-              point[0] * 1.1 * noiseScaleX,
-              p.frameCount * 0.001 * noiseScaleY
-            );
-            if (point[1] < p.windowHeight * 1.1) {
-              atLeastOneOnScreen = true;
-            }
-          });
-          p.endShape();
-          if (!atLeastOneOnScreen) {
-            reset();
-          }
-        }
-      };
       this.setState({ sketch });
     }
   }
@@ -115,10 +116,9 @@ class IndexPage extends Component {
           <Canvas>
             {this.state.sketch && <this.P5Wrapper sketch={this.state.sketch} />}
           </Canvas>
-          <BiddefordSeal />
+          {/* <BiddefordSeal />
           <Box top={5 * this.state.scrollY} />
-          <div>uyo</div>
-          <SweetcreamLogo />
+          <SweetcreamLogo /> */}
         </MainWrapper>
       </>
     );
