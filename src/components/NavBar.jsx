02676b338cf7623e9a409a1186caeaf91f3b4d83
Andrew Heppner <andrewbheppner@gmail.com>
2019-03-16 14:01:42 -0400

    Fix top padding of navbar.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const OFFWHITE = "#FFFFF0";
 
 const Header = styled.div`
   width: 100%;
-  height: 5%;
+  height: 50px;
   position: fixed;
   top: 0:
   left: 0;
--- src/components/styledElements.jsx
+++ src/components/styledElements.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const SceneWrapper = styled.div`
   position: relative;
   display: flex;
+  padding-top: 50px;
   height: 100vh;
   width: 100vw;
   background: ${props => props.backgroundColor};
